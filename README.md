fa4ea26d079418e2345872da5e8901a05875fbce
P3TERX <25927179+P3TERX@users.noreply.github.com>
2021-02-09 08:34:25 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# wireguard-go-builder
+
+This repository contains a bash script for compiling the [wireguard-go](https://git.zx2c4.com/wireguard-go/) binary from source. With this binary, users are able to create WireGuard sessions without installing the kernel module.
+
+[![GitHub release (latest by date)](https://img.shields.io/github/v/release/P3TERX/wireguard-go-builder?style=for-the-badge&label=Download)](https://github.com/P3TERX/wireguard-go-builder/releases/latest)
+
+## License
+
+    Copyright (C) 2017-2021 WireGuard LLC. All Rights Reserved.
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy of
+    this software and associated documentation files (the "Software"), to deal in
+    the Software without restriction, including without limitation the rights to
+    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+    of the Software, and to permit persons to whom the Software is furnished to do
+    so, subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
---
+++ build.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+# ref: https://github.com/AlexanderOMara/wireguard-go-builds/blob/master/build.sh
+
+set -e
+set -u
+
+version="${WGGO_VERSION}"
+src_file_sha256="${WGGO_SRC_SHA}"
+
+src_file="wireguard-go-${version}.tar.xz"
+src_url="https://git.zx2c4.com/wireguard-go/snapshot/${src_file}"
+
+base_dir="$PWD"
+vendor_dir="$base_dir/vendor"
+src_dir="$base_dir/src"
+build_dir="$base_dir/build"
+src_file_path="$vendor_dir/$src_file"
+
+# Remove existing build directories (add write access to src directories to avoid permission issues).
+chmod -R u+w "$src_dir" || true
+rm -rf "$vendor_dir" "$src_dir" "$build_dir"
+mkdir "$vendor_dir" "$src_dir" "$build_dir"
+
+# Get and verify extract source file.
+wget -O "$src_file_path" "$src_url"
+echo "$src_file_sha256  $src_file_path" | shasum -c
+tar --strip-components=1 -C "$src_dir" -xJf "$src_file_path"
+
+# Patch Makefile to remove building on Linux check.
+sed -i.bak 's/$(wildcard .git),linux/$(wildcard .git),linux_check_disabled/g' "$src_dir/Makefile"
+
+# Build all the targets.
+targets=(
+	'darwin amd64'
+
+	'linux 386'
+	'linux amd64'
+	'linux arm'
+	'linux arm64'
+	'linux ppc64'
+	'linux ppc64le'
+	'linux mips'
+	'linux mipsle'
+
+	'freebsd 386'
+	'freebsd amd64'
+	'freebsd arm'
+
+	'openbsd amd64'
+)
+for target in "${targets[@]}"; do
+	target_=($target)
+	target_os="${target_[0]}"
+	target_arch="${target_[1]}"
+	build_archive_file="wireguard-go-$target_os-$target_arch.tar.xz"
+	build_archive_path="$build_dir/$build_archive_file"
+	build_archive_file_sha256="$build_archive_file.sha256"
+
+	echo '------------------------------------------------------------'
+	echo "Building: $target_os $target_arch"
+	echo '------------------------------------------------------------'
+
+	export GOOS="$target_os"
+	export GOARCH="$target_arch"
+
+	pushd "$src_dir" > /dev/null
+	make
+	tar cfJ "$build_archive_path" wireguard-go*
+
+	pushd "$build_dir" > /dev/null
+	shasum -a 256 "$build_archive_file" > "$build_archive_file_sha256"
+	cat "$build_archive_file_sha256"
+	popd > /dev/null
+
+	make clean
+
+	popd > /dev/null
+done
